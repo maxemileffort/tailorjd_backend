4d42936f1d82c600fc473dc46050a1f04c758755
Max Wood <maxemileffort@gmail.com>
2025-03-19 21:47:14 -0500

    added bullet point rewriter

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ if (isProd) {
 
 // Setting some required headers that I guess have been missing this whole time
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://tailorjd.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   // res.header('Access-Control-Allow-Credentials', 'true');
